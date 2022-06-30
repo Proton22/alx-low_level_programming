6a5ceb7b9a9aa1964e31aa13af668a4ddc0edbc6
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-06-30 15:44:56 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - print variuos type 
- * Return: 1 if the command exist
+ * main - print a standard error
+ *
+ * Return: return 1
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
