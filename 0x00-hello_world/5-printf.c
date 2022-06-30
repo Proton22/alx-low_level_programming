8173468e6dd95219381fcd0dc5451715229faed2
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-06-30 12:01:17 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Printing out a functiom
+ * Return: 0 if it is correct
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
+
