4107ab8d56a04c65940544942266e41077e845c1
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-06-30 15:01:17 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print variuos type 
+ * Return: 1 if the command exist
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
