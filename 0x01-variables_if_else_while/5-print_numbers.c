6817c8589f277449004c71398dd74173537a1b0b
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-02 13:51:22 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print number base
+ * Return: 0 if executed properly
+ */
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+printf("%d\n", num);
+}
+return (0);
+}
