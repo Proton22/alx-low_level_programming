e34f8630d8d9e6a0312fd03e9eeb70362c766aca
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-02 07:27:55 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print letters alphabetically
+ * Return: 0 if executed properly
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++);
+{
+printf("%c", alpha);
+}
+return (0);
+}
