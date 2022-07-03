fc5a44419108400bb08818601dba9384b23f9e06
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-03 16:27:19 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,13 @@
 int main(void)
 {
 int d1, d2;
-for (d1 = 0; d1 <= 10; d1++)
+for (d1 = 0; d1 < 10; d1++)
 {
-for (d2 = 0; d2 <= 10; d2++)
+for (d2 = 0; d2 < 10; d2++)
 {
 putchar((d1 % 10) + '0');
 putchar((d2 % 10) + '0');
-if (d1 == 9 && d2 == 9)
-continue;
+if (d1 <= 9 && d2 <= 9)
 putchar(',');
 putchar(' ');
 }
