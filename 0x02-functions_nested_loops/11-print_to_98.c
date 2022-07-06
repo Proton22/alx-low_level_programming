022f684de61def570851f959a4c220aa7765ce9b
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-06 13:30:04 +0100

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_to_98 - Print natural number
+ * @n: a character
+ */
+void print_to_98(int n)
+{
+for (n >= 0; n <= 98; n++)
+{
+_putchar(n);
+_putchar(',');
+_putchar(' ');
+}
+_purchar('\n');
+return;
+}
