4f0eaa408cbac359d6da13109aa85d901bb94da7
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-08 10:06:45 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - print all digit
+ * Return: 1 if c is a digit otherwise 0
+ * @c: a character
+ */
+int _isdigit(int c)
+{
+if (c = 0 && c < 10)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
