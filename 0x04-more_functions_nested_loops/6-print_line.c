aa42ba50a5e41cd7dfc9204d5b9e4bcd2e275cfc
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-08 14:43:42 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_line - print out number
+ */
+void print_line(int n)
+{
+int x;
+if (n <= 0)
+_putchar('\n');
+for (x = 0; x <= n; x++)
+_putchar('_');
+_putchar('\n')
+}
