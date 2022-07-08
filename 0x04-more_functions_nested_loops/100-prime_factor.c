2ab622f638bfff6bc3ec9ee662ebabfddb34c87f
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-08 15:42:22 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a number
+ * Return: if executed
+ */
+int main(void)
+{
+long x, y;
+x = 612852475143;
+for (y = 2; x > y; y++)
+{
+while (x % y == 0)
+{
+x = x
+}
+}
+printf("%lu", y);
+putchar('\n');
+return (0);
+}
