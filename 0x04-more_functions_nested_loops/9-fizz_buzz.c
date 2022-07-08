7b1b21cf252b44577ff214c06c6298229c028630
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-08 15:24:12 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - print number
+ * Return: if executed properly
+ */
+int main(void)
+{
+int x;
+for (x = 1; x <= 100; x++)
+{
+if (x % 3 == 0 || x % 5 == 0)
+{
+printf("Fizz");
+}
+if (x % 5 == 0)
+{
+printf("Buzz");
+}
+}
+else
+{
+printf("%d", x);
+}
+if (x != 100)
+{
+putchar(' ');
+}
+}
