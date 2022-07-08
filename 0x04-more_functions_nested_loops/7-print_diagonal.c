7b88a0f2f282da3bb6ec220beb65ec793a6d11e5
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-08 15:11:34 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - print number diagonally
+ * @n: a character
+ */
+void print_diagonal(int n)
+{
+int x, y;
+if (n > 0)
+{
+for (x = 1; x <= n; x++)
+{
+for (y = 1; y <= n; y++)
+{
+if (x == y)
+{
+_putchar(92);
+break;
+}
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
