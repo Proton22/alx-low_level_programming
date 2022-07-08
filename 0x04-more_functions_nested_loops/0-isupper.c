947ee862e9efb49e6f7b9d9ac3b6504113032f91
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-08 09:29:47 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - print the number
+ * Return: 1 if c is uppercase otherwise 0
+ */
+int _isupper(int c)
+{
+if (c = 'A'; c <= 'Z'; c++)
+{
+_putchar(c);
+return (1);
+}
+else
+{
+return (0);
+}
+}
