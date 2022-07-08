0c794032e4777872916ed42de35979181dc4f1db
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-08 15:37:21 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - print number
+ * @size: a character
+ */
+void print_triangle(int size)
+{
+int x, y;
+if (size > 0)
+{
+for (x = 1; x <= size; x++)
+{
+for (y = 1; y <= size; y++)
+{
+if (y <= size - x)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
