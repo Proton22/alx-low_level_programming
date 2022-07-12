fbfd425454c84512b9f2515c3e5846c564a18c29
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-12 10:15:26 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - print the length
+ * @s: a character argument
+ * Return: if executed properly
+ */
+int _strlen(char *s)
+{
+int l;
+s = "Point";
+l = strlen(s);
+_putchar(l);
+return (0);
+}
