49ce9e13d022466b2309189f307bfd323d3f8fd4
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-12 10:05:22 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - print function that swap two integer
+ * @a: first character
+ * @b: second character
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
