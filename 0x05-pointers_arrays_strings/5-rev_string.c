83ed3ca99e025c9c1f38754b7a2ebfac469def40
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-12 12:19:28 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,10 +5,18 @@
  */
 void rev_string(char *s)
 {
-int n;
-n = 0;
-for (*s = n -1; *s >= 0; s--)
+int c, c1, c2;
+char tmp;
+c1 = 0;
+c2 = 0;
+while (s[c1] != '\0')
+c1++;
+c2 = c1 -1;
+for (c  = 0; c < c1 / 2; c++)
 {
-_putchar(*s);
+tmp = s[c];
+s[c] = s[c2];
+s[c2] = tmp;
+c2 -= 1;
 }
 }
