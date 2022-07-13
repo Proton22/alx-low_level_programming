5959644c0e8bd399ac83c3b5e65734020fa49687
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-13 15:35:47 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print array
+ * @n: a character
+ * @a: a character form
+ */
+void print_array(int *a, int n)
+{
+int j;
+for (j = 0; j < n; j++)
+{
+printf("%d", a[j]);
+if (j != (n - 1))
+{
+printf(" , ");
+}
+}
+printf('\n');
+}
