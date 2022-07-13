13d9d314bd0d881ace726faf2edc0bedba24d29d
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-13 10:11:15 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - print series of numbers
+ * @str: a character
+ */
+void puts2(char *str)
+{
+int i, len;
+len = strlen(str);
+for (i = len; i < len && i >= 0; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
