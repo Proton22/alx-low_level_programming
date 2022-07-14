9e9bd885999149265bd3276541e968cf4a5a8682
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-14 07:36:59 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * reverse_array - print a reverse
  * @a: a character
@@ -7,17 +6,17 @@
  */
 void reverse_array(int *a, int n)
 {
-int i, length1, length2;
+char tmp;
+int i, length1;
 length1 = 0;
-length2 = 0;
-while (s[length1] != '\0')
+while (a[length1] != '\0')
 length1++;
-length2 = length1 - 1;
+n = length1 - 1;
 for (i = 0; i < length1 / 2; i++)
 {
-tmp = s[i];
-s[i] = s[length2];
-s[length2] = tmp;
-length2 -= 1;
+tmp = a[i];
+a[i] = a[n];
+a[n] = tmp;
+n -= 1;
 }
 }
