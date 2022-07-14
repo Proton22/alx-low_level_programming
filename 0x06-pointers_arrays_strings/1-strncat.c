b28ba0d4a10667d0cc55a7b34f1d5d4f3bb0fbf8
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-14 05:57:40 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncat - print a concantenate
+ * @dest: a string
+ * @src: a character
+ * @n: a word
+ * Return: if properly executed
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+while(dest[i])
+i++;
+for (n = 0; src[n] < n; n++)
+{
+dest[i] =src[n];
+i++;
+}
+return (dest);
+}
