1deaa28877543c74e73ee1bb346237b6657b92aa
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-14 07:33:03 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,4 +6,18 @@
  * @n: a character string
  */
 void reverse_array(int *a, int n)
-
+{
+int i, length1, length2;
+length1 = 0;
+length2 = 0;
+while (s[length1] != '\0')
+length1++;
+length2 = length1 - 1;
+for (i = 0; i < length1 / 2; i++)
+{
+tmp = s[i];
+s[i] = s[length2];
+s[length2] = tmp;
+length2 -= 1;
+}
+}
