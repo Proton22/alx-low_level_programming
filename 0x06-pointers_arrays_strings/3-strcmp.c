caeb1ea1e0f228143cc21a13c4d4343906771fba
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-14 07:03:41 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - print the character
+ * @s1: a character
+ * @s2: a character
+ * Return: if executed properly
+ */
+int _strcmp(char *s1, char *s2)
+{
+strcmp(s1, s2);
+return (s1);
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <string.h>
+/**
+ * reverse_array - print a reverse
+ * @a: a character
+ * @n: a character string
+ */
+void reverse_array(int *a, int n)
+
