2ffc7dc672d484414605b4918ec34dd2ae8c889d
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-14 08:00:57 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - print character
+ * @z: a character
+ */
+char *string_toupper(char *x)
+{
+int i;
+i = 0;
+while (x[i])
+{
+if (x[i] >= 97 && x[i] <= 122; i++)
+{
+x[i] = x[i] - 32;
+}
+}
+return (x[i]);
+}
