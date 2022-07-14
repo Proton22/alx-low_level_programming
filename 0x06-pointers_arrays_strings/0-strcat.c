798dc9951f2af92634dfca76e281431123eb9af4
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-14 05:27:13 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,12 @@ char *_strcat(char *dest, char *src)
 {
 int i, j;
 i = 0;
-while (dest[i] != '\0')
+while (dest[i])
 i++;
-for (j = 0, src[j] != '\0'; j++)
+for (j = 0, src[j] != 0; j++)
 {
 dest[i] = src[j];
+i += 1;
 }
 dest[i] = '\0';
 return (dest);
