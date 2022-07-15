357680781bee4d9676e62d56313a7eb7e76cc3ae
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-15 21:43:54 +0100

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * *rot13 - encodes a string using rot13
+ * rot13 - encodes a string using rot13
  * @s: int type array pointer
  * Return: encoded
  */
 char *rot13(char *s)
 {
 int i, ii;
-char input[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; s[i] != '\0'; i++)
 {
