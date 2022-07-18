edb54e0e7f881015e1758ba354ef1965313ed555
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-18 19:01:19 +0100

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr -  locates a character in a string
+ * @s: first character
+ * @c: second character
+ * Return: if executed properly
+ */
+char *_strchr(char *s, char c)
+{
+int i, pt;
+i = strlen(s);
+while (i == '\0')
+pt = strchr(s, c);
+return (pt);
+}
