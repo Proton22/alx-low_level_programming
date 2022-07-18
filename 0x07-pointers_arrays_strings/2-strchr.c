ddb55b47e46c755c6919ae5452601dbbf555f1c7
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-18 19:07:13 +0100

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,9 +8,11 @@
  */
 char *_strchr(char *s, char c)
 {
-int i, pt;
-i = strlen(s);
-while (i == '\0')
-pt = strchr(i, c);
-return (pt);
+unsigned int i = 0;
+for (; *(s + i) != '\0'; i++)
+if (*(s + i) == c)
+return (s + i);
+if (*(s + i) == c)
+return (s + i);
+return ('\0');
 }
