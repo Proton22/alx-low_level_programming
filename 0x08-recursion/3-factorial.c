5387079215343debc098aa463e78222dfa9adc55
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-20 19:37:04 +0100

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - Write a function that returns the factorial of a given number
+ * @n: a character
+ * Return: if executed properly
+ */
+int factorial(int n)
+{
+while (n < 0)
+{
+return (-1);
+if (n == 0 || n == 1)
+return 1;
+return n * factorial(n - 1);
+}
