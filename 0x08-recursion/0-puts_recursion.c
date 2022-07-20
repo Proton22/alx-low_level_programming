59e1fde1f89fd7fd10edd80e427fa3e31279631b
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-20 11:05:27 +0100

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts_recursion -  prints a string, followed by a new line
+ * @s: a character
+ */
+void _puts_recursion(char *s)
+{
+int a;
+a = strlen(s);
+_putchar(s);
+_putchar('\n');
+}
