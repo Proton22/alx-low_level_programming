f13f8b95351019110e8775ca69e973d33648ad52
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-20 12:10:16 +0100

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,8 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-if (s != '\0')
+if (*s == '\0')
 {
-return (s);
-return (_puts_recursion(s + 1));
+_putchar('\n');
+return;
+}
+_putchar(s);
+s++;
+_puts_recursion(s);
 }
