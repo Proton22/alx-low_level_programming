c0e937e08dd4584f9974c0b5c75ae87a07e872c2
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-24 23:56:33 +0100

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Write a program that prints the number of arguments
+ * @argc: a character
+ * @argv: a character
+ * Return: if executed properly 
+ */
+int main(int argc, char *argv[]);
+{
+printf("%d\n", argc - 1);
+return (0);
+}
