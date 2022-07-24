49d4791edd2b908e1aa874e4dea54a17a1fc6297
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-25 00:12:09 +0100

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: a character
  * Return: if executed properly 
  */
-int main(int argc, char *argv[]);
+int main(int argc, char __attribute__((__unused__))  *argv[]);
 {
 printf("%d\n", argc - 1);
 return (0);
