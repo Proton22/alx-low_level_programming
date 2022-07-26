4d23ec07268ac06ecc0cade9b191475808e7329e
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-27 00:20:47 +0100

    Task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - Write a function that concatenates all the arguments
+ * @ac: an integer
+ * @av: a character
+ * Return: if executed properly
+ */
+char *argstostr(int ac, char **av)
+{
+int k = 0, i = ac, j, sum = 0, l = 0;
+char *s;
+if (ac == 0 || av == NULL)
+return (NULL);
+while (ac--)
+sum += (len(av[ac]) + 1);
+s = (char *) malloc(sum + 1);
+if (s != NULL)
+{
+while (k < i)
+{
+for (j = 0; av[k][j] != '\0'; j++)
+s[j + l] = av[k][j];
+s[l + j] = '\n';
+l += (j + 1);
+k++;
+}
+s[l] = '\0';
+}
+else
+{
+return (NULL);
+}
+return (s);
+}
+/**
+ * len - returns length of string
+ * @str: string counted
+ * Return: returns the length
+ */
+int len(char *str)
+{
+int len = 0;
+if (str != NULL)
+{
+while (str[len])
+len++;
+}
+return (len);
+}
