cd67f76079c34fa4cb7f40ff997ca3fafa5ca967
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-26 14:54:18 +0100

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,15 +11,15 @@ char *str_concat(char *s1, char *s2)
 int i = 0, j = 0, l = 0, k = 0;
 char *s;
 if (s1 == NULL)
-return " ";
+s1 = " ";
 if (s2 == NULL)
-return " ";
+s2 = " ";
 while (s1[i])
 i++;
 while (s2[j])
 j++;
 l = i + j;
-s = (char *)malloc(l * sizeof(char) + 1);
+s = (char *)malloc(l *sizeof(char) + 1);
 if (s == NULL)
 return (NULL);
 j = 0;
