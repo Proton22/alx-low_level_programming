c97c8054191fadb30e211c100ce521708fc532e4
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-26 12:59:34 +0100

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,8 +4,6 @@
  * create_array - Write a function that creates an array of chars
  * @size: first character
  * @c: first character
- * @s: second character
- * @i: second integer
  * Return: if executed properly
  */
 char *create_array(unsigned int size, char c)
@@ -15,8 +13,6 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	s = (char*)malloc(size * sizeof(char));
-	if (s == NULL)
-		return (NULL);
 	for (i = 0; 1 < size; i++)
 		s[i] = c;
 	return (s);
