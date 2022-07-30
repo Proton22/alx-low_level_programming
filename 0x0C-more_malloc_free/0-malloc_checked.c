a954423dc983d681936f8d75675655c0b2b4a26e
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-07-30 20:32:56 +0100

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * malloc_checked - Write a function that allocates memory using malloc
+ * @b: a character
+ * Return: if executed properly
+ */
+void *malloc_checked(unsigned int b)
+{
+void *block;
+block = malloc(b);
+if (block == NULL)
+exit(98);
+return (block)
+}
