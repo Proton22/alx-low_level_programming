5495530a098a3c233d4429496febb84aa38e90e0
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-08-03 09:07:34 +0100

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - Write a function that initialize a variable of type
+ * @d: struct type
+ * @name: first character
+ * @age: a float type
+ * @owner: second character
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
+}
