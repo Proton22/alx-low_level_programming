b6b6e76a4e2ef2a90ac503171f1fd3af0ce41a16
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-08-04 21:41:02 +0100

    Task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * get_op_func - contain the function that selects the correct function to perform
+ * @s: a character
+ * Return: if executed properly
+ */
+int (*get_op_func(char *s))(int, int)
+{
+ op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].op != NULL)
+{
+if(*s == *ops[i].op)
+retorn(ops[i].f);
+i++;
+}
+return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+/**
+ * op_add - write a function that adds
+ * @a: first integer
+ * @b: second integer
+ * Return: if executed properly
+ */
+int op_add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
+/**
+ * op_sub - write a function that adds
+ * @a: first integer
+ * @b: second integer 
+ * Return: if executed properly
+ */
+int op_sub(int a, int b);
+{
+return (a - b);
+}
+/**
+ * op_mul - write a function that multiply
+ * @a: first integer
+ * @b: second integer
+ * Return: if executed properly
+ */
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+/**
+ * op_div - write a function that divide
+ * @a: first integer
+ * @b: second integer
+ * Return: if executed properly
+ */
+int op_div(int a, int b);
+{
+	return(a / b);
+}
+/**
+ * op_mod - write a function that get remainder
+ * @a: first integer
+ * @b: second integer
+ * Return: if executed properly
+ */
+int op_mod(int a, int b);
+{
+	return(a % b);
+}
