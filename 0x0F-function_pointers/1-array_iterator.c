a24bdd06bf0e59c698903642acb0e33c081f6beb
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-08-04 14:42:28 +0100

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
-	if (array != NULL && action != NULL)
-	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
-	}
+unsigned long int i;
+if (array != NULL && action != NULL)
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
 }
