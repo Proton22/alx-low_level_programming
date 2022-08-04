46c4b1ddce5b78f0c7a7e7bc8b096b8908c13668
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-08-04 15:12:40 +0100

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ if (size <= 0)                                                                  
 if (array != NULL && cmp != NULL)
 for (i = 0; i < size; i++);
 {
-if (cmp(array[i]));
+if (cmp(array[i]))
 {
 return (i);
 }
