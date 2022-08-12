2f3c267cb4b3541ca1b797958886b520b8bd67d3
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-08-12 17:58:01 +0100

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,15 +7,14 @@
 size_t print_list(const list_t *h)
 {
 size_t cont = 0;
-while (h != NULL)  
+while (h)  
 {
-if (h->str != NULL)
-printf("[%d] %s\n", h->len, h->str);
+if (h->str)
+printf("[%u] %s\n", h->len, h->str);
 else
-printf("[%d] (nil)\n", h->len);
+printf("[0] (nil)\n");
 cont++;
 h = h->next;
-cont++;
 }
 return (cont);
 }
