ced96aae7a9beec5880df210cdd940d588b4c68d
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-08-16 19:18:51 +0100

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,14 +6,19 @@
 void free_listint2(listint_t **head)
 {
 listint_t *temp;
-if (*head == NULL)
+if (*head != NULL)
 {
-return (NULL);
-}
 while (*head)
 {
-temp = *head->next;
+temp = *head;
+temp = (*head)->next;
 free(*head);
-*head = temp;
 }
+}
+else
+{
+	return;
+}
+free(*head);
+head = 0;
 }
