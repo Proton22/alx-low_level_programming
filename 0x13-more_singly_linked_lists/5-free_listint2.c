6bce6f9a37d9ee548eb6a7dbe99288e6d1d5457b
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-08-16 19:12:25 +0100

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - Write a function that frees a listint_t list
+ * @head: a list of variables
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+if (*head == NULL)
+{
+return (NULL);
+}
+while (*head)
+{
+temp = *head->next;
+free(*head);
+*head = temp;
+}
+}
