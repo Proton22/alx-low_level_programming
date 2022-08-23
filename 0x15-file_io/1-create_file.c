715a786bb6f3667a7667aad7afb2dae981173331
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-08-23 14:24:01 +0100

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ return (s);
  * create_file - Create a function that creates a file
  * @filename: a constant character
  * @text_content: a character of string
- * Returns: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
