fd0f7334a8088a350d182079bf546e9f9e735a00
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-08-23 15:00:42 +0100

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,12 +12,12 @@ int _strlen(char *c)
 	return (s);
 }
 /**
- * create_file - Create a function that creates a file
+ * append_text_to_file - Create a function that creates a file
  * @filename: a constant character
  * @text_content: a character of string
  * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	if (filename == NULL)
