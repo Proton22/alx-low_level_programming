7c6337e06025907c2857aadd40328bcfc78507b0
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-08-23 14:13:10 +0100

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strlen - a string of length
+ * @c: a character string
+ * Return: length of a string
+ */
+int _strlen(char *c)
+{
+int s = 0;
+if (c[s])
+s++;
+return (s);
+}
+/**
+ * create_file - Create a function that creates a file
+ * @filename: a constant character
+ * @text_content: a character of string
+ * Returns: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+if (filename == NULL)
+{
+return (-1);
+}
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+write(fd, text_content, _strlen(text_content));
+close(fd);
+return (1);
+}
