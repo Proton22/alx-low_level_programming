b57032a4d83ce65b5c0a46338d9850541adad76e
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-10-25 22:35:36 +0100

    Task Done

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,5 @@
-#ifndef _HASH_TABLES_H_
-#define _HASH_TABLES_H_
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -38,6 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
 /**
  * struct shash_node_s - Node of a sorted hash table
  * @key: The key, string
@@ -79,4 +80,4 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
-#endif
+#endif /* HASH_TABLES_H */
