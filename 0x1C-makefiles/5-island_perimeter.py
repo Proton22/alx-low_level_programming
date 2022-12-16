ddc61423906bf6a5d42e4c1a60742df11e544f0e
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-12-16 13:50:42 +0100

    Task done

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,6 +6,7 @@ def num_water_neighbors(grid, i, j):
     """Returns the number of water neighbors a cell has in a grid."""
 
     num = 0
+
     if i <= 0 or not grid[i - 1][j]:
         num += 1
     if j <= 0 or not grid[i][j - 1]:
@@ -15,8 +16,11 @@ def num_water_neighbors(grid, i, j):
     if i >= len(grid) - 1 or not grid[i + 1][j]:
         num += 1
     return num
+
+
 def island_perimeter(grid):
     """Returns the perimeter of the island in grid."""
+    
     perim = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
